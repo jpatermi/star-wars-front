a120647482debba8df850e6774ebb2fbbda3679b
JP <paterninaj@gmail.com>
2023-10-13 11:30:19 -0300

    feat: Add mobile sidebar

--- app/(welcome)/layout.tsx
+++ app/(welcome)/layout.tsx
@@ -1,4 +1,4 @@
-// import { Navbar } from "./_components/navbar";
+import { Navbar } from "./_components/navbar";
 import { Sidebar } from "./_components/sidebar";
 
 const DashboardLayout = ({
@@ -8,13 +8,13 @@ const DashboardLayout = ({
 }) => {
   return ( 
     <div className="h-full">
-      {/*<div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">*/}
-      {/*  <Navbar />*/}
-      {/*</div>*/}
+      <div className="h-[70px] md:pl-56 fixed inset-y-0 w-full z-50">
+        <Navbar />
+      </div>
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
-      <main className="md:pl-56 pt-[80px] h-full">
+      <main className="md:pl-56 pt-[90px] h-full">
         {children}
       </main>
     </div>
--- lib/starWarsApi.ts
+++ lib/starWarsApi.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const starWarsApi = axios.create({
-  baseURL: 'https://swapi.dev/api'
-  // baseURL: 'http://localhost:3008/api/star-wars'
+  // baseURL: 'https://swapi.dev/api' // Direct to API
+  baseURL: 'http://localhost:3008/api/star-wars' // Through the back end
 });
 
 export default starWarsApi;
